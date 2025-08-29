43bb62779ed4eaaadf3fb3870890536e88a57929
Anton Miasoedov <anton.myasoedov@gmail.com>
2025-08-28 22:19:02 -0400

    initial commit for Project: Real-Time Trade Feed Quality Gate

---
+++ spark_jobs/generate_trades.py
@@ -0,0 +1,27 @@
+import json
+import random
+import string
+import time
+import uuid
+from datetime import datetime, timedelta
+from kafka import KafkaProducer
+
+
+
+
+
+TOPIC = "trades.raw.v1"
+BOOTSTRAP_SERVERS = "localhost:9092"
+
+SYMBOLS = ["RY.TO", "TD.TO", "BNS.TO", "BMO.TO", "CM.TO"]
+CURRENCIES = ["CAD", "USD", "EUR", "GBP"]  # CAD/USD valid, rest = invalid
+BROKERS = ["BRK-001", "BRK-002", "BRK-011", "BRK-020"]
+
+
+
+
+producer = KafkaProducer(
+    bootstrap_servers=BOOTSTRAP_SERVERS,
+    value_serializer=lambda v:json.dumps(v).encode("utf-8").encode("UTF-8")
+
+)
